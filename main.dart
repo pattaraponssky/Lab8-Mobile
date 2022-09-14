8e5f627f2b6c05375361418bc2c5463b91ebf654
pattaraponssky <113360594+pattaraponssky@users.noreply.github.com>
2022-09-14 11:47:06 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_lab8/homepage.dart';
+import 'package:flutter_application_lab8/login.dart';
+import 'package:flutter_application_lab8/register.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return buildMaterialApp();
+  }
+
+  MaterialApp buildMaterialApp() {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/homepage': (context) => Homepage()
+      },
+    );
+  }
+}
